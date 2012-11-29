de9c4144a5ef407604fd52d047b24efc2d08e97c
Oleg N. Cher <allot@bk.ru>
2012-11-29 18:57:10 +0200

    Add script '+Browse.bat' to generate html-interfaces of modules.

---
+++ Src/Oberon/J2me (GPCP)/+Browse.bat
@@ -0,0 +1,6 @@
+@CALL cprun Browse -html -sort GrCfg.cps
+@CALL cprun Browse -html -sort GrScr.cps
+@CALL cprun Browse -html -sort GrTiles.cps
+@CALL cprun Browse -html -sort Rsrc.cps
+@CALL cprun Browse -html -sort Dash
+@PAUSE
