b3b0f1954089b2abeb77c7856787707adadf4f1a
Oleg N. Cher <allot@bk.ru>
2015-10-11 15:43:51 +0300

    / ZXSpec/GrTiles.PutTile: fast OR compact variants (thanks to jerri).

--- ZXSpec/C/GrCfg.h
+++ ZXSpec/C/GrCfg.h
@@ -7,7 +7,9 @@
 #define GrCfg_ScreenTable  0xFC  /* This memory #FC00; #FD00 used for screen table. */
 
 //#define GrCfg_CheckTileCoords
+//#define GrCfg_FastTiles
 #define GrCfg_UseSound
+
 
 #define GrCfg__init()
 
--- ZXSpec/C/GrTiles.c
+++ ZXSpec/C/GrTiles.c
@@ -16,6 +16,19 @@ void GrTiles_PutTile (unsigned char x, unsigned char y, unsigned char* tile) {
   END
 */
   __asm
+#ifdef __test_atrib__
+    LD HL,#0x5800
+    LD DE,#0x5801
+    LD BC,#0x100
+    LD (HL),#4*8
+    LDIR
+    LD BC,#0x100
+    LD (HL),#5*8
+    LDIR
+    LD BC,#0xFF
+    LD (HL),#6*8
+    LDIR
+#endif //__test_atrib__
     POP  BC
     POP  DE      ; D = y; E = x
     POP  HL      ; HL = tile address
@@ -28,7 +41,7 @@ void GrTiles_PutTile (unsigned char x, unsigned char y, unsigned char* tile) {
     RET  C
     CP   A,D     ; IF y <= 30 THEN RETURN
     RET  C
-#endif
+#endif //GrCfg_CheckTileCoords
     LD   A,D     ; y
     ADD  A
     ADD  D       ; * 6
@@ -40,87 +53,99 @@ void GrTiles_PutTile (unsigned char x, unsigned char y, unsigned char* tile) {
     LD   E,A     ; low screen addr byte
     INC  B
     LD   A,(BC)
+#ifndef GrCfg_FastTiles
     LD   D,A     ; high screen addr byte
 ; #1
-    LDI          ; LD (DE),(HL): INC HL: INC DE: DEC BC
     LDI
-    DEC  DE      ; calculate next screen addr
-    DEC  DE
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
     INC  D
 ; #2
-    LDI          ; LD (DE),(HL): INC HL: INC DE: DEC BC
     LDI
-    DEC  DE      ; calculate next screen addr
-    DEC  DE
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
     INC  D
     LD   A,D
     AND  #7
     CALL Z,DOWN_DE8$
 ; #3
-    LDI          ; LD (DE),(HL): INC HL: INC DE: DEC BC
     LDI
-    DEC  DE      ; calculate next screen addr
-    DEC  DE
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
     INC  D
 ; #4
-    LDI          ; LD (DE),(HL): INC HL: INC DE: DEC BC
     LDI
-    DEC  DE      ; calculate next screen addr
-    DEC  DE
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
     INC  D
     LD   A,D
     AND  #7
     CALL Z,DOWN_DE8$
 ; #5
-    LDI          ; LD (DE),(HL): INC HL: INC DE: DEC BC
     LDI
-    DEC  DE      ; calculate next screen addr
-    DEC  DE
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
     INC  D
 ; #6
-    LDI          ; LD (DE),(HL): INC HL: INC DE: DEC BC
     LDI
-    DEC  DE      ; calculate next screen addr
-    DEC  DE
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
     INC  D
     LD   A,D
     AND  #7
     CALL Z,DOWN_DE8$
 ; #7
-    LDI          ; LD (DE),(HL): INC HL: INC DE: DEC BC
     LDI
-    DEC  DE      ; calculate next screen addr
-    DEC  DE
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
     INC  D
 ; #8
-    LDI          ; LD (DE),(HL): INC HL: INC DE: DEC BC
     LDI
-    DEC  DE      ; calculate next screen addr
-    DEC  DE
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
     INC  D
     LD   A,D
     AND  #7
     CALL Z,DOWN_DE8$
 ; #9
-    LDI          ; LD (DE),(HL): INC HL: INC DE: DEC BC
     LDI
-    DEC  DE      ; calculate next screen addr
-    DEC  DE
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
     INC  D
 ; #10
-    LDI          ; LD (DE),(HL): INC HL: INC DE: DEC BC
     LDI
-    DEC  DE      ; calculate next screen addr
-    DEC  DE
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
     INC  D
     LD   A,D
     AND  #7
     CALL Z,DOWN_DE8$
 ; #11
-    LDI          ; LD (DE),(HL): INC HL: INC DE: DEC BC
     LDI
-    DEC  DE      ; calculate next screen addr
-    DEC  DE
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
     INC  D
 ; #12
     LDI          ; 16t
@@ -135,5 +160,330 @@ DOWN_DE8$:
     LD   A,D
     SUB  #8
     LD   D,A
+#else //GrCfg_FastTiles
+    BIT  1,D     ; y&2
+    JP   NZ,Y_MOD_2_3$
+Y_MOD_0_1$:
+    BIT  0,D     ; y&1
+    LD   D,A     ; high screen addr byte
+    JR   NZ,Y_MOD_1$
+Y_MOD_0$: // y = {0, 4, 8, 12, 16, 20, 24, 28}
+    LDI          ; #1
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #2
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #3
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #4
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #5
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #6
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #7
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #8
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    INC  D
+    LD   A,E
+    ADD  #32-1
+    LD   E,A
+    JR   C,Y20$
+    LD   A,D
+    SUB  #8
+    LD   D,A
+Y20$:
+    LDI          ; #9
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #10
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #11
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #12 ; 16t
+    LD   A,(HL)  ; 7t
+    LD   (DE),A  ; 7t
+    RET
+Y_MOD_1$: // y = {1, 5, 9, 13, 17, 21, 25, 29}
+    LDI          ; #1
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #2
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    INC  D
+    LD   A,E
+    ADD  #32-1
+    LD   E,A
+    JR   C,Y21$
+    LD   A,D
+    SUB  #8
+    LD   D,A
+Y21$:
+    LDI          ; #3
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #4
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #5
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #6
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #7
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #8
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #9
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #10
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    INC  D
+    LD   A,E
+    ADD  #32-1
+    LD   E,A
+    JR   C,Y9$
+    LD   A,D
+    SUB  #8
+    LD   D,A
+Y9$:
+    LDI          ; #11
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #12 ; 16t
+    LD   A,(HL)  ; 7t
+    LD   (DE),A  ; 7t
+    RET
+Y_MOD_2_3$:
+    BIT  0,D     ; y&1
+    LD   D,A     ; high screen addr byte
+    JR   NZ,Y_MOD_3$
+Y_MOD_2$: // y = {2, 6, 10, 14, 18, 22, 26, 30}
+    LDI          ; #1
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #2
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #3
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #4
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    INC  D
+    LD   A,E
+    ADD  #32-1
+    LD   E,A
+    JR   C,Y10$
+    LD   A,D
+    SUB  #8
+    LD   D,A
+Y10$:
+    LDI          ; #5
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #6
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #7
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #8
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #9
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #10
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #11
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #12 ; 16t
+    LD   A,(HL)  ; 7t
+    LD   (DE),A  ; 7t
+    RET
+Y_MOD_3$: // y = {3, 7, 11, 15, 19, 23, 27}
+    LDI          ; #1
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #2
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #3
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #4
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #5
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #6
+    LDI
+    LD   A,E
+    ADD  #32-2
+    LD   E,A
+    LD   A,D
+    SUB  #8-1
+    LD   D,A
+    LDI          ; #7
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #8
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #9
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #10
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #11
+    LD   A,(HL)
+    LD   (DE),A
+    INC  HL
+    DEC  E
+    INC  D
+    LDI          ; #12 ; 16t
+    LD   A,(HL)  ; 7t
+    LD   (DE),A  ; 7t
+#endif //GrCfg_FastTiles
   __endasm;
 } //GrTiles_PutTile
